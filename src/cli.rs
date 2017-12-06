f1ab37f9474cd01197ebd5b41519f631395af48a
panicbit <panicbit.dev@gmail.com>
2017-12-06 03:10:11 +0100

    Refactor aoc cli

--- src/cli.rs
+++ src/cli.rs
@@ -1,51 +1,71 @@
 use std::fmt::Display;
 use {Result, Client};
-use clap::{App,SubCommand};
+use clap::{App, SubCommand};
+
+struct Cli<'a, F, R> where
+    F: Fn(&str) -> R,
+    R: Display,
+{
+    event: &'a str,
+    day: u8,
+    level: u8,
+    code: F,
+    client: Client,
+}
+
+impl<'a, F, R> Cli<'a, F, R> where
+    F: Fn(&str) -> R,
+    R: Display,
+{
+    fn new(event: &'a str, day: u8, level: u8, code: F) -> Result<Self> {
+        let session_token = ::get_session_token()?;
+        Ok(Self {
+            event,
+            day,
+            level,
+            code,
+            client: Client::new(event, session_token)?,
+        })
+    }
+
+    fn run(&self) -> Result<()> {
+        let cli = App::new(format!("Advent of Code {} - Day {} part {}", self.event, self.day, self.level))
+            .subcommand(SubCommand::with_name("submit")
+                .about("Submit the solution")
+            )
+            .get_matches();
+
+        match cli.subcommand() {
+            ("submit", _) => self.submit(),
+            _ => self.default(),
+        }
+    }
+
+    fn default(&self) -> Result<()> {
+        let input = self.client.get_input(self.day)?;
+        let result = (self.code)(&input);
+
+        println!("Result: '{}'", result);
+
+        Ok(())
+    }
+
+    fn submit(&self) -> Result<()> {
+        let input = self.client.get_input(self.day)?;
+        let result = (self.code)(&input).to_string();
+
+        println!("Submitting '{}' for AoC {} day {} part {}", result, self.event, self.day, self.level);
+
+        self.client.submit_solution(self.day, self.level, &result)?;
+
+        Ok(())
+    }
+}
 
 pub fn run<F, R>(event: &str, day: u8, level: u8, code: F) where
     F: Fn(&str) -> R,
     R: Display,
 {
-    let cli = App::new(format!("Advent of Code {} - Day {} part {}", event, day, level))
-        .subcommand(SubCommand::with_name("submit")
-            .about("Submit the solution")
-        )
-        .get_matches();
-    
-    match cli.subcommand() {
-        ("submit", _) => submit(event, day, level, code).unwrap(),
-        _ => default(event, day, code).unwrap(),
-    }
-}
-
-fn default<F, R>(event: &str, day: u8, code: F) -> Result<()> where
-    F: Fn(&str) -> R,
-    R: Display,
-{
-    let input = client(event)?.get_input(day)?;
-    let result = code(&input);
-
-    println!("Result: '{}'", result);
-
-    Ok(())
-}
-
-fn submit<F, R>(event: &str, day: u8, level: u8, code: F) -> Result<()> where
-    F: Fn(&str) -> R,
-    R: Display,
-{
-    let client = client(event)?;
-    let input = client.get_input(day)?;
-    let result = code(&input).to_string();
-
-    println!("Submitting '{}' for AoC {} day {} part {}", result, event, day, level);
-
-    client.submit_solution(day, level, &result)?;
-
-    Ok(())
-}
-
-fn client(event: &str) -> Result<Client> {
-    let session_token = ::get_session_token()?;
-    Client::new(event.to_string(), session_token)
+    let app = Cli::new(event, day, level, code).unwrap();
+    app.run().unwrap();
 }
